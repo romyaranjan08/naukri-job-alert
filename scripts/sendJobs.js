2ab63ba02063240f4f93e3d56f588d3863920a19
Romya Ranjan Rout <romyaranjan08@gmail.com>
2026-03-12 00:31:31 +0530

    first commit

---
+++ scripts/sendJobs.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import fs from "fs";
+import { getJobs } from "../lib/naukri.js";
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
+
+async function sendJobs() {
+  const jobs = await getJobs();
+
+  const sentJobs = JSON.parse(fs.readFileSync("./data/sentJobs.json"));
+
+  const newJobs = jobs.filter((job) => !sentJobs.includes(job.link));
+
+  if (!newJobs.length) {
+    console.log("No new jobs");
+    return;
+  }
+
+  let message = "🚀 New React Jobs\n\n";
+
+  newJobs.forEach((job, i) => {
+    message += `${i + 1}. ${job.title}\n${job.link}\n\n`;
+  });
+
+  await axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+    chat_id: CHAT_ID,
+    text: message,
+  });
+
+  const updated = [...sentJobs, ...newJobs.map((j) => j.link)];
+
+  fs.writeFileSync("./data/sentJobs.json", JSON.stringify(updated, null, 2));
+
+  console.log("Jobs sent");
+}
+
+sendJobs();
